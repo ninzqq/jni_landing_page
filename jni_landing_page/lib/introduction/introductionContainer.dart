02dd1d9d373a39257fb17c6c6d393e311f7c117b
Janne Niinisalo <janne.niinisalo@gmail.com>
2024-12-10 22:35:50 +0200

    Some testinks

--- jni_landing_page/lib/introduction/introductionContainer.dart
+++ jni_landing_page/lib/introduction/introductionContainer.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/link.dart';
 
 class IntroductionContainer extends StatelessWidget {
   const IntroductionContainer({super.key});
+
+  void _launchImage() async {
+    final Uri url = Uri.parse('/assets/images/ktm.jpg');
+    if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +21,15 @@ class IntroductionContainer extends StatelessWidget {
         children: [
           Padding(
             padding: const EdgeInsets.all(20.0),
-            child: Image.asset(
-              'assets/images/ktm.jpg',
-              scale: 6,
+            child: Link(
+              uri: Uri.parse('/assets/images/ktm.jpg'),
+              target: LinkTarget.blank, // Opens in a new tab
+              builder: (BuildContext context, FollowLink? followLink) {
+                return Image.asset(
+                  'assets/images/ktm.jpg',
+                  scale: 6,
+                );
+              },
             ),
           ),
           const Text(
